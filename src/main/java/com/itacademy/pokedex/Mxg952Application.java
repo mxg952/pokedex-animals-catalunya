ac57e764571e1deaf166094235430e7d11f75de3
mxg952 <mguillempa@proton.me>
2025-11-01 11:15:27 +0100

    first commit

---
+++ src/main/java/com/itacademy/pokedex/Mxg952Application.java
@@ -0,0 +1,13 @@
+package com.itacademy.pokedex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mxg952Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mxg952Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/itacademy/pokedex/Mxg952ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itacademy.pokedex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mxg952ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
